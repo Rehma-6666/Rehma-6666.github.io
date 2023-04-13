45b5c5fcf6cc608fa15988824c7efb1b42aceca8
Rehma-6666 <123839611+Rehma-6666@users.noreply.github.com>
2023-04-13 00:23:46 -0400

    up

---
+++ Mcluhan.js/Mcluhan.js
@@ -0,0 +1,19 @@
+console.log("Mcluhan");
+
+const theButton = document.querySelector('button');
+let theBody = document.querySelector('body');
+let theThesis = document.getElementById("theMainPoint")
+
+theButton.addEventListener('click', beTheChange )
+
+function beTheChange(){
+    console.log("you clicked!");
+   
+   
+}
+function beTheChange(){
+    console.log("you clicked!");
+    theThesis.style.backgroundColor = "green";
+    theBody.style.backgroundColor = "green"
+   
+}
